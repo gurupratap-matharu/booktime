b3ffb067e944a502207d72afa9adbdbc7e94cb97
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-14 19:29:26 -0300

    first basic unit test written

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestPage(TestCase):
+    def test_home_page_works(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'BookTime')
