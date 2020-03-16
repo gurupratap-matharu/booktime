6b7e282c1eaaf951e40d197f65772e33014646ba
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-16 14:37:45 -0300

    class based view added for contact form

--- main/views.py
+++ main/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+
+from main import forms
 
 
 def home(request):
     return render(request, "home.html", {})
+
+
+class ContactUsView(FormView):
+    template_name = "contact_form.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
