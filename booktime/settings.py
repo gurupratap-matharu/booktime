5c2c5dd96835f863e5cc2534f3ccb8a62d771d58
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-16 14:11:48 -0300

    added email configuration into settings

--- booktime/settings.py
+++ booktime/settings.py
@@ -147,3 +147,15 @@ LOGGING = {
         },
     },
 }
+
+if not DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST_USER = "username"
+    EMAIL_HOST = 'smtp.domain.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_PASSWORD = "password"
+else:
+    EMAIL_BACKEND = (
+        'django.core.mail.backends.console.EmailBackend'
+    )
