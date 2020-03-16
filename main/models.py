15d10ede1c9243eeaca30989a31e7e284a201c1c
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-16 15:06:13 -0300

    added the first product model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    slug = models.SlugField(max_length=48)
+    active = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    date_updated = models.DateTimeField(auto_now=True)
