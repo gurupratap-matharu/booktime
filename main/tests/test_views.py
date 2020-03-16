1d31ce8dc034c13e6444e0de9c44a17d671bb379
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-16 14:08:54 -0300

    added reverse function to remove hardcoded urls from the test cases

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class TestPage(TestCase):
     def test_home_page_works(self):
-        response = self.client.get("/")
+        response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'BookTime')
+
+    def test_about_page_works(self):
+        response = self.client.get(reverse("about_us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about_us.html')
         self.assertContains(response, 'BookTime')
