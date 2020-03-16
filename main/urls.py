0ecf3ba3dd82127dd06e06df27a6bab066806d15
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-16 14:37:32 -0300

    added contact-us as a new endpoint

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from main import views
+
 urlpatterns = [
+    path("contact-us/", views.ContactUsView.as_view(), name="contact_us"),
     path("about-us/", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
 ]
