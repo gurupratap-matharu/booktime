68acf1cb2e505855bfbd81f2646915de97b39ff3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-16 16:23:03 -0300

    added thumbnail field to ProductImage

--- main/models.py
+++ main/models.py
@@ -9,3 +9,17 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="product-images")
+    thumbnail = models.ImageField(upload_to="product-thumbnails", null=True)
+
+
+class ProductTag(models.Model):
+    products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
