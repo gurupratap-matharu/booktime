bc3c31a6b8875cf326638835662b128f38899f90
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-16 14:37:17 -0300

    added a new test to check rendering of contact us page

--- main/tests/test_forms.py
+++ main/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.core import mail
 from django.test import TestCase
+from django.urls import reverse
 
 from main import forms
 
@@ -24,3 +25,10 @@ class TestForm(TestCase):
             'message': "Hi there"
         })
         self.assertFalse(form.is_valid())
+
+    def test_contact_us_page_works(self):
+        response = self.client.get(reverse("contact-us"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main/contact_form.html')
+        self.assertContains(response, 'BookTime')
+        self.assertIsInstance(response.context["form"], forms.ContactForm)
