6de99bd13344e1af34ccf58bd9146b1ab5877973
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-17 17:09:24 -0300

    add more functionality to the admin interface

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'in_stock', 'price')
+    list_filter = ('active', 'in_stock', 'date_updated')
+    list_editable = ('in_stock', )
+    search_fields = ('name', )
+    prepopulated_fields = {"slug": ("name",)}
+
+
+admin.site.register(models.Product, ProductAdmin)
