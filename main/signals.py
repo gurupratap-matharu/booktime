0f441f5b84510e702bca3899b6cdd3345032b2f3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-20 15:09:26 -0300

    corrected spelling for thumbnail

--- main/signals.py
+++ main/signals.py
@@ -14,16 +14,16 @@ logger = logging.getLogger(__name__)
 
 @receiver(pre_save, sender=ProductImage)
 def generate_thumbnail(sender, instance, **kwargs):
-    logger.info("Generating thumbail for product {}".format(instance.product.id))
+    logger.info("Generating thumbnail for product %d", instance.product.id,)
     image = Image.open(instance.image)
     image = image.convert("RGB")
-    image.thumbail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+    image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
 
     temp_thumb = BytesIO()
     image.save(temp_thumb, "JPEG")
     temp_thumb.seek(0)
 
     # set save=False else it'll run in an infinite loop
-    instance.thumbail.save(instance.image.name, ContentFile(temp_thumb.read()),
-                           save=False)
+    instance.thumbnail.save(instance.image.name, ContentFile(temp_thumb.read()),
+                            save=False)
     temp_thumb.close()
