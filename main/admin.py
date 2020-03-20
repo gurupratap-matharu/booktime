badb2319756206b6b2dcea5b397cc90e892c8b02
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-20 12:07:48 -0300

    added producttag and productimage to the admin interface

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from . import models
 
@@ -12,3 +13,31 @@ class ProductAdmin(admin.ModelAdmin):
 
 
 admin.site.register(models.Product, ProductAdmin)
+
+
+class ProductTagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_filter = ('active', )
+    search_fields = ('name',)
+    prepopulated_fields = {"slug": ("name",)}
+    autocomplete_fields = ('products',)
+
+
+admin.site.register(models.ProductTag, ProductTagAdmin)
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ('thumbnail_tag', 'product_name')
+    readonly_fields = ('thumbnail',)
+    search_fields = ('product__name',)
+
+    def thumbnail_tag(self, obj):
+        if obj.thumbnail:
+            return format_html('<img src="%s"/>' % obj.thumbnail.url)
+        return "-"
+
+    def product_name(self, obj):
+        return obj.product.name
+
+
+admin.site.register(models.ProductImage, ProductImageAdmin)
