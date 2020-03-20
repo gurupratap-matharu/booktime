10711d6e79ad0a36a15a5592ab882464bfac44b7
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-20 18:29:15 -0300

    created product tag manager

--- main/models.py
+++ main/models.py
@@ -6,6 +6,24 @@ class ActiveManager(models.Manager):
         return self.filter(active=True)
 
 
+class ProductTagManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
+
+
+class ProductTag(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def natural_key(self):
+        return (self.slug,)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
@@ -14,19 +32,15 @@ class Product(models.Model):
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
     date_updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(ProductTag, blank=True)
 
     objects = ActiveManager()
+
+    def __str__(self):
+        return self.name
 
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="product-images")
     thumbnail = models.ImageField(upload_to="product-thumbnails", null=True)
-
-
-class ProductTag(models.Model):
-    products = models.ManyToManyField(Product, blank=True)
-    name = models.CharField(max_length=32)
-    slug = models.SlugField(max_length=48)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
