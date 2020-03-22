0564fe1bbcf80c83e1de61c550f2b1460a558eb1
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 19:51:00 -0300

    added endpoint to signup

--- main/urls.py
+++ main/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("products/<slug:tag>/", views.ProductListView.as_view(), name="products"),
     path("product/<slug:slug>/", DetailView.as_view(model=models.Product), name="product"),
+    path("signup/", views.SignupView.as_view(), name="signup"),
 ]
