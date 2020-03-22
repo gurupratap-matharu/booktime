9b7bc60fe0051754b1a94578949b2731487b3a97
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 18:06:01 -0300

    added product detail view to urls

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 
-from main import views
+from main import models, views
 
 urlpatterns = [
     path("contact-us/", views.ContactUsView.as_view(), name="contact_us"),
     path("about-us/", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("products/<slug:tag>/", views.ProductListView.as_view(), name="products"),
+    path("product/<slug:slug>/", DetailView.as_view(model=models.Product), name="product"),
 ]
