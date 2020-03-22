957d0d4342f9336279283d930cc7c539460a1056
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 16:31:40 -0300

    added list view for products

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import FormView
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import FormView, ListView
 
-from main import forms
+from main import forms, models
 
 
 def home(request):
@@ -16,3 +16,21 @@ class ContactUsView(FormView):
     def form_valid(self, form):
         form.send_mail()
         return super().form_valid(form)
+
+
+class ProductListView(ListView):
+    template_name = "main/product_list.html"
+    paginate_by = 4
+
+    def get_queryset(self):
+        tag = self.kwargs['tag']
+        self.tag = None
+        if tag != "all":
+            self.tag = get_object_or_404(models.ProductTag, slug=tag)
+
+        if self.tag:
+            products = models.Product.objects.active().filter(tag=self.tag)
+        else:
+            products = models.Product.objects.active()
+
+        return products.order_by("name")
