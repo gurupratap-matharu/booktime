31cf6abf6f932cc870d9ce898421d997f8d52abf
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 18:44:43 -0300

    added AUTH_USER_MODEL config setting

--- booktime/settings.py
+++ booktime/settings.py
@@ -112,8 +112,10 @@ USE_I18N = True
 
 USE_L10N = True
 
+
 USE_TZ = True
 
+AUTH_USER_MODEL = "main.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
