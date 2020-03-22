8205044267186ecd4dacab635c4eceb76e2ad85e
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 16:31:09 -0300

    added product/slug endpoint to urls

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("contact-us/", views.ContactUsView.as_view(), name="contact_us"),
     path("about-us/", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("products/<slug:tag>/", views.ProductListView.as_view(), name="products"),
 ]
