076541a77576c110b091498feb71231d639d22bc
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 19:51:17 -0300

    class based view to handle sign up for new users

--- main/views.py
+++ main/views.py
@@ -1,7 +1,13 @@
+import logging
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import FormView, ListView
 
 from main import forms, models
+
+logger = logging.getLogger(__name__)
 
 
 def home(request):
@@ -33,3 +39,26 @@ class ProductListView(ListView):
         else:
             products = models.Product.objects.active()
         return products.order_by("name")
+
+
+class SignupView(FormView):
+    template_name = "signup.html"
+    form_class = forms.UserCreationForm
+
+    def get_success_url(self):
+        redirect_to = self.request.GET.get("next", "/")
+        return redirect_to
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        form.save()
+        email = form.cleaned_data.get("email")
+        raw_password = form.cleaned_data.get("password1")
+        logger.info("New signup for email=%s through SignupView", email)
+        user = authenticate(email=email, password=raw_password)
+        login(self.request, user)
+
+        form.send_mail()
+
+        messages.info(self.request, "You signed up successfully.")
+        return response
