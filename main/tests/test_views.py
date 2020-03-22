4ae88f912e8e430dbc68bf666273e44060f0dbb1
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 20:33:37 -0300

    two new tests to verify if user signup view is working correctly

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,9 +1,11 @@
 from decimal import Decimal
+from unittest.mock import patch
 
+from django.contrib import auth
 from django.test import TestCase
 from django.urls import reverse
 
-from main import models
+from main import forms, models
 
 
 class TestPage(TestCase):
@@ -60,3 +62,24 @@ class TestPage(TestCase):
         product_list = (models.Product.objects.active().filter(tags__slug="opensource").order_by("name"))
 
         self.assertEqual(list(product_list), list(response.context['object_list']))
+
+    def test_user_signup_page_loads_correctly(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "signup.html")
+        self.assertContains(response, "BookTime")
+        self.assertIsInstance(response.context["form"], forms.UserCreationForm)
+
+    def test_user_signup_page_submission_works(self):
+        post_data = {
+            "email": "user@domain.com",
+            "password1": "abcabcabc",
+            "password2": "abcabcabc",
+        }
+        with patch.object(forms.UserCreationForm, "send_mail") as mock_send:
+            response = self.client.post(reverse("signup"), post_data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(models.User.objects.filter(email="user@domain.com").exists())
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        mock_send.assert_called_once()
