eab49ff4d25a00def315f22da3dd71cfbc33aa36
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 19:50:44 -0300

    added User creation form in the forms.py

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,33 @@
 import logging
 
 from django import forms
+from django.contrib.auth.forms import \
+    UserCreationForm as DjangoUserCreationForm
+from django.contrib.auth.forms import UsernameField
 from django.core.mail import send_mail
 
+from . import models
+
 logger = logging.getLogger(__name__)
+
+
+class UserCreationForm(DjangoUserCreationForm):
+
+    class Meta(DjangoUserCreationForm.Meta):
+        model = models.User
+        fields = ("email",)
+        field_classes = {"email": UsernameField}
+
+    def send_mail(self):
+        logger.info("Sending signup email for email=%s", self.cleaned_data["email"],)
+        message = "Welcome{}".format(self.cleaned_data["email"])
+        send_mail(
+            "Welcome to BookTime",
+            message,
+            "site@booktime.domain",
+            [self.cleaned_data["email"]],
+            fail_silently=True,
+        )
 
 
 class ContactForm(forms.Form):
