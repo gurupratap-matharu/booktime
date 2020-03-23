f801226932db6465144f0dfc2e495b72637cce4d
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-23 16:41:25 -0300

    added endpoints for each address path

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,10 @@ from django.views.generic.detail import DetailView
 from main import forms, models, views
 
 urlpatterns = [
+    path("address/", views.AddressListView.as_view(), name="address_list",),
+    path("address/create/", views.AddressCreateView.as_view(), name="address_create",),
+    path("address/<int:pk>/", views.AddressUpdateView.as_view(), name="address_update",),
+    path("address/<int:pk>/delete/", views.AddressDeleteView.as_view(), name="address_delete",),
     path("contact-us/", views.ContactUsView.as_view(), name="contact_us"),
     path("about-us/", TemplateView.as_view(template_name="about_us.html"), name="about_us"),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
@@ -14,4 +18,5 @@ urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name="login.html",
                                                 form_class=forms.AuthenticationForm,), name="login"),
+
 ]
