58ab10c3838b6074791ae2d96c1153c15cd974cd
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-23 16:41:45 -0300

    added CRUD methods for manipulating user address

--- main/views.py
+++ main/views.py
@@ -2,8 +2,12 @@ import logging
 
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse_lazy
 from django.views.generic import FormView, ListView
+from django.views.generic.edit import (CreateView, DeleteView, FormView,
+                                       UpdateView)
 
 from main import forms, models
 
@@ -62,3 +66,53 @@ class SignupView(FormView):
 
         messages.info(self.request, "You signed up successfully.")
         return response
+
+
+class AddressListView(LoginRequiredMixin, ListView):
+    model = models.Address
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class AddressCreateView(LoginRequiredMixin, CreateView):
+    model = models.Address
+    fields = [
+        "name",
+        "address1",
+        "address2",
+        "zip_code",
+        "city",
+        "country",
+    ]
+    success_url = reverse_lazy("address_list")
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.user = self.request.user
+        obj.save()
+        return super().form_valid(form)
+
+
+class AddressUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Address
+    fields = [
+        "name",
+        "address1",
+        "address2",
+        "zip_code",
+        "city",
+        "country",
+    ]
+    success_url = reverse_lazy("address_list")
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class AddressDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Address
+    success_url = reverse_lazy("address_list")
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
