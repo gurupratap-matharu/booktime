3260dbbc9d756d33f374495e849052182745fdc1
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-22 22:06:28 -0300

    created a new authentication form

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 import logging
 
 from django import forms
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import \
     UserCreationForm as DjangoUserCreationForm
 from django.contrib.auth.forms import UsernameField
@@ -39,3 +40,27 @@ class ContactForm(forms.Form):
         message = "From: {0}\n{1}".format(self.cleaned_data['name'], self.cleaned_data['message'])
         send_mail("Site message", message, "site@booktime.domain",
                   ["customerservice@booktime.domain"], fail_silently=False)
+
+
+class AuthenticationForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(strip=False, widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user = None
+        super().__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+
+        if email is not None and password:
+            self.user = authenticate(self.request, email=email, password=password)
+            if self.user is None:
+                raise forms.ValidationError("Invalid email/password combination.")
+            logger.info("Authentication successful for email=%s", email)
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user
