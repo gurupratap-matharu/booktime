ca50a7ec0fa7d7888eadff9d585e5e973eb20340
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-23 17:13:29 -0300

    unit tests for checking the address update and read functionality

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -83,3 +83,42 @@ class TestPage(TestCase):
         self.assertTrue(models.User.objects.filter(email="user@domain.com").exists())
         self.assertTrue(auth.get_user(self.client).is_authenticated)
         mock_send.assert_called_once()
+
+    def test_address_list_page_returns_only_owned(self):
+        user1 = models.User.objects.create_user("user1", "pw432joij")
+        user2 = models.User.objects.create_user("user2", "ps432joij")
+        models.Address.objects.create(
+            user=user1,
+            name="john kimball",
+            address1="flat2",
+            address2="12 Stralz avenue",
+            city="London",
+            country="uk",
+        )
+        models.Address.objects.create(
+            user=user2,
+            name="marc kimball",
+            address1="123 Decan road",
+            address2="London",
+            country="uk",
+        )
+        self.client.force_login(user2)
+        response = self.client.get(reverse("address_list"))
+        self.assertEqual(response.status_code, 200)
+
+        address_list = models.Address.objects.filter(user=user2)
+        self.assertEqual(list(address_list), list(response.context["object_list"]))
+
+    def test_address_create_stores_user(self):
+        user1 = models.User.objects.create_user("user1", "pw432joij")
+        post_data = {
+            "name": "john kercher",
+            "address1": "1 av st",
+            "address2": "",
+            "zip_code": "MA12GS",
+            "city": "Manchester",
+            "country": "uk",
+        }
+        self.client.force_login(user1)
+        self.client.post(reverse("address_create"), post_data)
+        self.assertTrue(models.Address.objects.filter(user=user1).exists())
