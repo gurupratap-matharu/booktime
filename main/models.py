9d160900302bb92c2c44a85cf2dc254c79ab60bf
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-23 20:53:41 -0300

    added basket and basketline to models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -112,3 +113,26 @@ class Address(models.Model):
                 self.country,
             ]
         )
+
+
+class Basket(models.Model):
+    OPEN = 10
+    SUBMITTED = 20
+    STATUSES = (
+        (OPEN, "Open"),
+        (SUBMITTED, "Submitted"),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    status = models.IntegerField(choices=STATUSES, default=OPEN)
+
+    def is_empty(self):
+        return self.basketline_set.all().count() == 0
+
+    def count(self):
+        return sum(i.quantity for i in self.basketline_set.all())
+
+
+class BasketLine(models.Model):
+    basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1, validators=[MinValueValidator(1)])
