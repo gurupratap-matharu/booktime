c4af63e5890caaca0ec7e6315997de7221d42c18
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-26 19:18:35 -0300

    wrote logic to merge basket when user logs in

--- main/signals.py
+++ main/signals.py
@@ -1,12 +1,13 @@
 import logging
 from io import BytesIO
 
+from django.contrib.auth.signals import user_logged_in
 from django.core.files.base import ContentFile
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from PIL import Image
 
-from .models import ProductImage
+from .models import Basket, ProductImage
 
 THUMBNAIL_SIZE = (300, 300)
 logger = logging.getLogger(__name__)
@@ -27,3 +28,23 @@ def generate_thumbnail(sender, instance, **kwargs):
     instance.thumbnail.save(instance.image.name, ContentFile(temp_thumb.read()),
                             save=False)
     temp_thumb.close()
+
+
+@receiver(user_logged_in)
+def merge_baskets_if_found(sender, user, request, **kwargs):
+    anonymous_basket = getattr(request, "basket", None)
+
+    if anonymous_basket:
+        try:
+            loggedin_basket = Basket.objects.get(user=user, status=Basket.OPEN)
+            for line in anonymous_basket.basketline_set.all():
+                line.basket = loggedin_basket
+                line.save()
+            anonymous_basket.delete()
+            request.basket = loggedin_basket
+            logger.info("Merged basket to id %d", loggedin_basket.id)
+
+        except Basket.DoesNotExist:
+            anonymous_basket.user = user
+            anonymous_basket.save()
+            logger.info("Assigned user to basket id %d", anonymous_basket.id)
