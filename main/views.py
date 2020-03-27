6e4077a51100b392011bb8dc309d20f1be6b78d0
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-27 19:25:47 -0300

    new views to user django_filters and django-tables

--- main/views.py
+++ main/views.py
@@ -1,14 +1,18 @@
 import logging
 
+import django_filters
+from django import forms as django_forms
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db import models as django_models
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import FormView, ListView
 from django.views.generic.edit import (CreateView, DeleteView, FormView,
                                        UpdateView)
+from django_filters.views import FilterView
 
 from main import forms, models
 
@@ -171,3 +175,32 @@ class AddressSelectionView(LoginRequiredMixin, FormView):
         basket.create_order(form.cleaned_data['billing_address'],
                             form.cleaned_data['shipping_address'])
         return super().form_valid(form)
+
+
+class DateInput(django_forms.DateInput):
+    input_type = 'date'
+
+
+class OrderFilter(django_filters.FilterSet):
+    class Meta:
+        model = models.Order
+        fields = {
+            'user__email': ['icontains'],
+            'status': ['exact'],
+            'date_updated': ['gt', 'lt'],
+            'date_added': ['gt', 'lt'],
+        }
+        filter_overrides = {
+            django_models.DateTimeField: {
+                'filter_class': django_filters.DateFilter,
+                'extra': lambda f: {'widget': DateInput}
+            }
+        }
+
+
+class OrderView(UserPassesTestMixin, FilterView):
+    filterset_class = OrderFilter
+    login_url = reverse_lazy("login")
+
+    def test_func(self):
+        return self.request.user.is_staff is True
