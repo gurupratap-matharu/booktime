e3cec175e85ce6ea8a46a9ff5a7c1af939438d2b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-27 12:25:15 -0300

    added try except block for edge cases

--- main/middlewares.py
+++ main/middlewares.py
@@ -5,7 +5,10 @@ def basket_middleware(get_response):
     def middleware(request):
         if 'basket_id' in request.session:
             basket_id = request.session['basket_id']
-            basket = models.Basket.objects.get(id=basket_id)
+            try:
+                basket = models.Basket.objects.get(id=basket_id)
+            except models.Basket.DoesNotExist:
+                basket = None
             request.basket = basket
         else:
             request.basket = None
