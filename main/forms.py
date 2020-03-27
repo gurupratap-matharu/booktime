79231850b15c5a5f96bd44748b9c45a9bae4e7c1
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-27 11:16:49 -0300

    some minor bug corrections while rendering forms

--- main/forms.py
+++ main/forms.py
@@ -73,3 +73,14 @@ BasketLineFormSet = inlineformset_factory(models.Basket,
                                           extra=0,
                                           widgets={"quantity": widgets.PlusMinusNumberInput()}
                                           )
+
+
+class AddressSelectionForm(forms.Form):
+    billing_address = forms.ModelChoiceField(queryset=None)
+    shipping_address = forms.ModelChoiceField(queryset=None)
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        queryset = models.Address.objects.filter(user=user)
+        self.fields['billing_address'].queryset = queryset
+        self.fields['shipping_address'].queryset = queryset
