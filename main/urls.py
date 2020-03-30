a95f23c04bbfa5ef67e1f5cb532f30defc0bdf23
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-30 14:46:31 -0300

    added api endpoint and router configurations to urls

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
+from rest_framework import routers
 
-from main import forms, models, views
+from main import endpoints, forms, models, views
+
+router = routers.DefaultRouter()
+router.register(r'orderlines', endpoints.PaidOrderLineViewSet)
+router.register(r'orders', endpoints.PaidOrderViewSet)
 
 urlpatterns = [
     path("address/", views.AddressListView.as_view(), name="address_list",),
@@ -23,4 +28,5 @@ urlpatterns = [
     path("order/done/", TemplateView.as_view(template_name="order_done.html"), name="checkout_done"),
     path("address-select/", views.AddressSelectionView.as_view(), name="address_select"),
     path("order-dashboard/", views.OrderView.as_view(), name="order_dashboard",),
+    path("api/", include(router.urls)),
 ]
