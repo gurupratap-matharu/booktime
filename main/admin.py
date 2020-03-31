e5e48b1d03e01b54b98dd2d96dbe1df860f2c79b
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-31 12:31:17 -0300

    added new interfaces for different types of admin

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,17 @@
+import logging
+from datetime import datetime, timedelta
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.db.models import Avg, Count, Min, Sum
+from django.db.models.functions import TruncDay
+from django.template.response import TemplateResponse
+from django.urls import path
 from django.utils.html import format_html
 
 from . import models
+
+logger = logging.getLogger(__name__)
 
 
 @admin.register(models.User)
@@ -57,6 +66,16 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     autocomplete_fields = ('tags',)
 
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.readonly_fields
+        return list(self.readonly_fields) + ["slug", "name"]
+
+    def get_prepopulated_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.prepopulated_fields
+        return {}
+
 
 admin.site.register(models.Product, ProductAdmin)
 
@@ -66,6 +85,16 @@ class ProductTagAdmin(admin.ModelAdmin):
     list_filter = ('active', )
     search_fields = ('name',)
     prepopulated_fields = {"slug": ("name",)}
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.readonly_fields
+        return list(self.readonly_fields) + ["slug", "name"]
+
+    def get_prepopulated_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.prepopulated_fields
+        return {}
 
 
 admin.site.register(models.ProductTag, ProductTagAdmin)
@@ -80,6 +109,7 @@ class ProductImageAdmin(admin.ModelAdmin):
         if obj.thumbnail:
             return format_html('<img src="%s"/>' % obj.thumbnail.url)
         return "-"
+    thumbnail_tag.short_description = "Thumbnail"
 
     def product_name(self, obj):
         return obj.product.name
@@ -114,28 +144,101 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineInline,)
     fieldsets = (
         (None, {
-            "fields": ("user", "status")
+            "fields": (("user", "status"))
         }),
         (
             "Billing info",
             {
-                "fields": ("billing_name",
-                           "billing_address1",
-                           "billing_address2",
-                           "billing_zip_code",
-                           "billing_city",
-                           "billing_country",
+                "fields": (("billing_name",
+                            "billing_address1",
+                            "billing_address2"),
+                           ("billing_zip_code",
+                            "billing_city",
+                            "billing_country")
                            )
             }),
         (
             "Shipping info", {
                 "fields": (
-                    "shipping_name",
-                    "shipping_address1",
-                    "shipping_address2",
-                    "shipping_zip_code",
-                    "shipping_city",
-                    "shipping_country",
+                    ("shipping_name",
+                     "shipping_address1",
+                     "shipping_address2",),
+                    ("shipping_zip_code",
+                     "shipping_city",
+                     "shipping_country",)
                 )},
         ),
     )
+
+
+class DispatchersProductAdmin(ProductAdmin):
+    readonly_fields = ("description", "price", "tags", "active")
+    prepopulated_fields = {}
+    autocomplete_fields = {}
+
+
+class ColoredAdminSite(admin.sites.AdminSite):
+    def each_context(self, request):
+        context = super().each_context(request)
+        context["site_header_color"] = getattr(self, "site_header_color", None)
+        context["module_caption_color"] = getattr(self, "module_caption_color", None)
+        return context
+
+
+class ReportingColoredAdminSite(ColoredAdminSite):
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path("order_per_day/", self.admin_view(self.orders_per_day),)
+        ]
+        return my_urls + urls
+
+    def orders_per_day(self):
+        return
+
+    def index(self, request, extra_content=None):
+        return
+
+
+class CentralOfficeOrderLineInline(admin.TabularInline):
+    model = models.OrderLine
+
+
+class CentralOfficeOrderAdmin(admin.ModelAdmin):
+    inlines = (CentralOfficeOrderLineInline,)
+
+
+class OwnersAdminSite(ReportingColoredAdminSite):
+    site_header = "BookTime owners administration"
+    site_header_color = "black"
+    module_caption_color = "grey"
+
+    def has_permission(self, request):
+        return (request.user.is_active and request.user.is_superuser)
+
+
+class CentralOfficeAdminSite(ReportingColoredAdminSite):
+    site_header = "BookTime central office administration"
+    site_header_color = "purple"
+    module_caption_color = "pink"
+
+    def has_permission(self, request):
+        return (
+            request.user.is_active and request.user.is_employee
+        )
+
+
+main_admin = OwnersAdminSite()
+main_admin.register(models.Product, ProductAdmin)
+main_admin.register(models.ProductImage, ProductImageAdmin)
+main_admin.register(models.ProductTag, ProductTagAdmin)
+main_admin.register(models.User, UserAdmin)
+main_admin.register(models.Basket, BasketAdmin)
+main_admin.register(models.Order, OrderAdmin)
+
+
+central_office_admin = CentralOfficeAdminSite("central-office-admin")
+central_office_admin.register(models.Product, ProductAdmin)
+central_office_admin.register(models.ProductTag, ProductTagAdmin)
+central_office_admin.register(models.ProductImage, ProductImageAdmin)
+central_office_admin.register(models.Order, CentralOfficeOrderAdmin)
