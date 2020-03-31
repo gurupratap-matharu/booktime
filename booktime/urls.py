0155f996d8a23ee0a4a9363ed53ce7d0ea20b2d3
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-03-31 12:48:13 -0300

    added dispatchers admin to urls.py

--- booktime/urls.py
+++ booktime/urls.py
@@ -22,6 +22,7 @@ from main import admin
 urlpatterns = [
     path('admin/', admin.main_admin.urls),
     path('office-admin/', admin.central_office_admin.urls),
+    path('dispatch-admin/', admin.dispatchers_admin.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
